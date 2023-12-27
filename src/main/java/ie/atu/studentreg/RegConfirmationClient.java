f14196b5a9e47fb7cefcf9285af4492063de245e
dewanhasan99 <g00410156@atu.ie>
2023-12-27 14:17:21 +0000

    Second Commit

---
+++ src/main/java/ie/atu/studentreg/RegConfirmationClient.java
@@ -0,0 +1,13 @@
+package ie.atu.studentreg;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+@FeignClient(name = "confirmation-service", url = "http://localhost:8083")
+public interface RegConfirmationClient {
+    @PostMapping("/confirmReg")
+    Map<String, String> confirmRegistration(@RequestBody StudentDetails studentDetails);
+}
